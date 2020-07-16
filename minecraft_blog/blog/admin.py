cf1fc7ed87c13a3ea5f519cd67f3580075b33b27
Paul <isakura313@gmail.com>
2020-07-16 17:33:05 +0300

    as

--- minecraft_blog/blog/admin.py
+++ minecraft_blog/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status') #какие свойства отображались
+    list_filter = ('status', 'created', 'publish', 'author') #по каким свойствам это все фильтруется
+    prepopulated_fields = {'slug' : ('title',)}  #связанные колонки
+    raw_id_fields = ('author',) #получаем информацию из БД База Данных
+    date_hierarchy = 'publish' # порядок от нового к старым
+    ordering = ('status', 'publish') #можно менять порядок по статусу и по публикации
+
 
 # Register your models here.
